d0ccf79eb650752e356c384790ed4be3d9ae7e2e
Curran D. Muhlberger <curran@muhlbergerweb.com>
2013-09-20 01:49:38 -0400

    Add RMS analyzer

--- src/main/scala/cdmuhlb/audioactors/Analyzer.scala
+++ src/main/scala/cdmuhlb/audioactors/Analyzer.scala
@@ -24,10 +24,32 @@ class MaxAnalyzer extends Analyzer {
 
   def report: Double = max
 }
+class RmsAnalyzer extends Analyzer {
+  var sqrSum: Double = 0.0
+  var nSamples: Int = 0
+
+  def process(samples: ShortBuffer): Unit = {
+    while (samples.hasRemaining) {
+      val a = samples.get().toDouble / Short.MaxValue.toDouble
+      sqrSum += a*a
+      nSamples += 1
+    }
+  }
+
+  def reset(): Unit = {
+    sqrSum = 0.0
+    nSamples = 0
+  }
+
+  def report: Double = math.sqrt(sqrSum/nSamples)
+}
 
 trait AnalyzerProducer {
   def produce(): Analyzer
 }
 object MaxAnalyzerProducer extends AnalyzerProducer {
   def produce(): Analyzer = new MaxAnalyzer
+}
+object RmsAnalyzerProducer extends AnalyzerProducer {
+  def produce(): Analyzer = new RmsAnalyzer
 }
